1f3bf5e2ad76f9f04f7b05c4cddb8fe446c7b0b3
vipin <mantrivipin17@gmail.com>
2019-04-07 16:38:44 +0530

    added form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('userDetail') userForm: NgForm;
+  subscribeValue: string = 'Basic';
+  user = {
+    email: '',
+    password: '',
+    subscription: ''
+  };
+  formSubmitted: boolean = false;
   title = 'template-form-assignment';
+
+  onSubmit() {
+    console.log(this.userForm);
+    this.formSubmitted = true;
+    this.user.email = this.userForm.value.email;
+    this.user.subscription = this.userForm.value.subscription;
+    this.user.password = this.userForm.value.password;
+    this.userForm.reset({
+      password: '',
+      email: '',
+      subscription: 'Basic'
+    });
+  }
 }
